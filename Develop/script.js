78c3acd967aa202ae5e2b323aae8be05654d932f
BraxB <braxb92@gmail.com>
2021-03-01 19:32:47 -0500

    added prompt asking how many characters the password should be and arrays of characters to be used in generating pw

--- Develop/script.js
+++ Develop/script.js
@@ -1,10 +1,15 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+// Write password to the #password input.
 function writePassword() {
+  var characters = prompt("How many characters would you like your password?");
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+
+  var caps = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  var lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var special = [" ", "!", '"', "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "]", "\\", "]", "^", "_", "`", "{", "|", "}", "~"]
 
   passwordText.value = password;
 
