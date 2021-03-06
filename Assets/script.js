ae4472b8f96546ea2969e1f7caedd223b745e247
BraxB <braxb92@gmail.com>
2021-03-06 14:22:57 -0500

    added validation to prevent passwords shorter than 8 or longer than 125 characters

--- Assets/script.js
+++ Assets/script.js
@@ -1,12 +1,14 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// create arrays of each type of character
 const uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 const lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 const special = [" ", "!", '"', "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "]", "\\", "]", "^", "_", "`", "{", "|", "}", "~"]
 const numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 
 function generatePassword(characters, upperConfirm, lowerConfirm, numbersConfirm, specialConfirm) {
+  // create empty array for available characters
   availChars = [];
 
   // add uppercase letters to array of available characters if the user confirms
@@ -29,8 +31,10 @@ function generatePassword(characters, upperConfirm, lowerConfirm, numbersConfirm
     availChars = availChars.concat(special);
   }
 
+  // set initial password variable to empty
   var password = "";
   
+  // select random characters from availabile characters for as many characters as specified by user
   for (i = 0; i < characters; i++) {
     let randIndex = Math.floor(Math.random() * availChars.length);
     password = password + availChars[randIndex];
@@ -42,6 +46,11 @@ function generatePassword(characters, upperConfirm, lowerConfirm, numbersConfirm
 // Write password to the #password input.
 function writePassword() {
   var characters = prompt("How many characters would you like your password?");
+  
+  // validation to prevent pw length shorter than 8 or larger than 125
+  while (characters < 8 || characters > 125) {
+    characters = prompt("Please enter a password length between 8 and 125");
+  }
   var upperConfirm = confirm("Include uppercase letters?");
   var lowerConfirm = confirm("Include lowercase letters?");
   var numbersConfirm = confirm("Include numbers?");
@@ -51,7 +60,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
